49eb18785ae6af21589b00faffbf6d7170cbd891
Sadık Çoban <43513351+sadik-coban@users.noreply.github.com>
2023-06-29 20:43:38 +0300

    Update App.js
    The search expression was changed.

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import useVideos from "../hooks/useVideos";
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const [videos, search] = useVideos("murat abi gf gta5");
+  const [videos, search] = useVideos("freecodecamp javascript");
 
   useEffect(() => {
     setSelectedVideo(videos[0]);
